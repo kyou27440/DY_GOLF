3dd95d43d10b13d5b3a43a4af6db5e7f8dc383c4
kyou27440 <kyou27440@users.noreply.github.com>
2026-07-28 09:39:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var APP_VERSION = 'v1.2.2';
+var APP_VERSION = 'v1.2.3';
 var SUPABASE_URL = 'https://qkkcugjuopjeuiyczjzf.supabase.co';
 var SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFra2N1Z2p1b3BqZXVpeWN6anpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ2NjE2NjQsImV4cCI6MjEwMDIzNzY2NH0.qPYwvuSBp_SEvi1vG4qoCIpbsBU1eTIYz43q-Df00DY';
 
